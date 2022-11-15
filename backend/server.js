43d4ee862617dc1532c132eb18493381d0a9b6a7
shadowKayn <pakobarbakadze@gmail.com>
2022-11-15 16:49:54 +0400

    Add orderRoutes to server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js "
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -13,6 +14,7 @@ app.use(express.json())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
