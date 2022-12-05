1a5b03aa96630187f955fc48e4d08ccf61f88da8
shadowKayn <pakobarbakadze@gmail.com>
2022-12-05 15:19:10 +0400

    Axios baseURL.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js ";
 //import path from "path";
 
@@ -12,6 +13,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import "./index.css";
 import App from "./App";
 
@@ -7,6 +8,8 @@ import { Provider } from "react-redux";
 import store from "./redux/index.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+
+axios.defaults.baseURL = 'https://best-tech-api.onrender.com/'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
